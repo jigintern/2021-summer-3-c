5b33a149dd88ef9b700e1fb77b8f2c5c39f7160f
kagura00 <wanwandog1018@gmail.com>
2021-08-31 16:58:56 +0900

    matching移動

--- src/Account.js
+++ src/Account.js
@@ -1,7 +1,7 @@
 import { DB } from "https://deno.land/x/sqlite/mod.ts";
 
 export class Account {
-    constructor(){
+    constructor() {
         this.db = new DB("account.db");
         this.db.query(`
             CREATE TABLE IF NOT EXISTS user_data (
@@ -13,7 +13,7 @@ export class Account {
             active TEXT
             )
         `);
-        
+
         this.db.query(`
             CREATE TABLE IF NOT EXISTS sports (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -22,49 +22,48 @@ export class Account {
             rate INTEGER,
             evaluation INTEGER
             )
-        `);  
+        `);
     }
 
-    get_db(){
+    get_db() {
         return this.db;
     }
 
-    async add(username,password){
-        this.db.query("INSERT INTO user_data (username,password,rate,evaluation) VALUES(?,?,?,?)",[username,password,0,0])
+    async add(username, password) {
+        this.db.query("INSERT INTO user_data (username,password,rate,evaluation) VALUES(?,?,?,?)", [username, password, 0, 0])
 
-        return "次のアカウントを追加しました：\"" + username +"\"";
+        return "次のアカウントを追加しました：\"" + username + "\"";
     }
 
-    findusername(username){
-        const userdata = this.get_db().query("SELECT * FROM user_data WHERE username = '" + username +"';");
+    findusername(username) {
+        const userdata = this.get_db().query("SELECT * FROM user_data WHERE username = '" + username + "';");
         var i = 0;
-        for(const u of userdata){
+        for (const u of userdata) {
             return null;
             i += 1;
         }
-        if(i == 0){
+        if (i == 0) {
             return userdata;
         }
 
     }
 
-    findaccounts(username,password){
-        const userdata = this.get_db().query("SELECT * FROM user_data WHERE username = '" + username +"' AND password = '"+ password + "';");
+    findaccounts(username, password) {
+        const userdata = this.get_db().query("SELECT * FROM user_data WHERE username = '" + username + "' AND password = '" + password + "';");
 
         var i = 0;
-        for (const u of userdata){
-            return userdata;  
+        for (const u of userdata) {
+            return userdata;
             i += 1;
         }
-        if(i == 0){
+        if (i == 0) {
             return null;
         }
     }
 
-    updatedata(tablename,id,key,value){
-        this.db.query("UPDATE" + tablename + "SET" + key + "='" + value +"';");
+    updatedata(tablename, id, key, value) {
+        this.db.query("UPDATE" + tablename + "SET" + key + "='" + value + "';");
 
-        this.db.query("UPDATE" + tablename + "SET" + key + "=" + value +";");
+        this.db.query("UPDATE" + tablename + "SET" + key + "=" + value + ";");
     }
 }
-    
