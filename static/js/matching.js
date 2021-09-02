65f5d52428aa2b736361bed89c0c91b2e693f4b8
kagura00 <wanwandog1018@gmail.com>
2021-09-02 10:27:56 +0900

    サーバー要求追加

--- static/js/matching.js
+++ static/js/matching.js
@@ -1,55 +1,50 @@
 //データベースに接続
 import { fetchJSON } from "https://js.sabae.cc/fetchJSON.js";
 
-//const UserId = sessionStorage.getItem('user_id');
-
 export function Matching(spo) { //マッチング
     del();//今ある表の初期化
-    const lists = await fetchJSON("api/matcing/find_userid_bysports", {
-        sport: sport.value,
+    const MyId = sessionStorage.getItem('userid');
+    const MyName = sessionStorage.getItem('username');
+    const MyRate = sessionStorage.getItem('rate');
+    const MyEvaluation = sessionStorage.getItem('evaluation');
+    const MyStatus = sessionStorage.getItem('status');
+    const MyPrefecture = sessionStorage.getItem('prefecture');
+    const MyCity = sessionStorage.getItem('city');
+    const td1 = document.createElement('td');
+    const td2 = document.createElement('td');
+    const td3 = document.createElement('td');
+
+
+    const UserIds = await fetchJSON("api/matcing/find_userid_bysports", {
+        sportsname: spo,
     });
-    /*
-    let tr1 = document.createElement('tr');
-    let td1 = document.createElement('td');
-    let td2 = document.createElement('td');
-    let td3 = document.createElement('td');
-    tr1.appendChild(td1);
-    td2.textContent = '××';
-    tr1.appendChild(td2);
-    td3.textContent = '▽△';
-    tr1.appendChild(td3);
-    document.getElementById("match").appendChild(tr1);
-*/
 
-
-    //データベースからデータを読み出す
-    /*const lists = db.queryEntries("select id from sports_table where sports = :sport", {
-        sport: "spo"
-    })*/
-
-    for (const list of lists) {
-        if (list.id == UserId)
+    for (const UserId of UserIds) {
+        if (UserId == MyId)
             continue;
-        let td1 = document.createElement('td');
-        let td2 = document.createElement('td');
-        let td3 = document.createElement('td');
-        td1.textContent = list.name;
+
+        const UserName = await fetchJSON("/api/account/findsports", {
+            userid: UserId,
+        });
+        const list = await fetchJSON("/api/account/findusername", {
+            username: UserName,
+        });
+        td1.textContent = list.username;
         tr.appendChild(td1);
-        td2.textContent = list.place;
+        td2.textContent = list.prefecture + list.city;
         tr.appendChild(td2);
-        td3.textContent = list.rate;
+        td3.textContent = list.evaluation;
         tr.appendChild(td3);
         table.appendChild(tr);
-    }
 
+    }
 }
 
 function del() {
-
     const table = document.getElementById('match');
     const row = table.rows.length;
+
     for (let i = row; i > 1; i--) {
         table.deleteRow(1);
     }
-
 }
