8c342362298552f1c67d67653783e9a4cfd2a0bb
temma <18419@toba-cmt.ac.jp>
2021-09-02 14:10:57 +0900

    細かい箇所の修正

--- src/Account.js
+++ src/Account.js
@@ -152,8 +152,6 @@ export class Account {
     matchcheck(userid){
         const matches = this.get_db().query("SELECT * FROM matchlog WHERE enemyid = " + userid + " AND status = 'false' ;");
         return matches;
+        
     }
-
-
-
 }
